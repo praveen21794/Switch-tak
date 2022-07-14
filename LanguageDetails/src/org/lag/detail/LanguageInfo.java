82d43b47b73fb5a69758624b9f90ec71b43739b8
praveen21794 <praveen21794@gmail.com>
2022-07-14 15:17:27 +0530

    Initial commit

---
+++ LanguageDetails/src/org/lag/detail/LanguageInfo.java
@@ -0,0 +1,27 @@
+package org.lag.detail;
+
+public class LanguageInfo {
+	public void tamilLanguage()
+	{
+	System.out.println("100");
+	
+	}
+	public void englishLanguage() {
+		System.out.println("200");
+	}
+	
+	public void hindiLanguge() {
+		System.out.println("300");
+
+		
+	}
+
+
+public static void main(String[]args) {
+	LanguageInfo s = new LanguageInfo();
+	
+	s.tamilLanguage();
+	s.englishLanguage();
+	s.hindiLanguge();
+}
+}
---
+++ LanguageDetails/src/org/lag/detail/StateDetails.java
@@ -0,0 +1,22 @@
+package org.lag.detail;
+//
+public class StateDetails extends LanguageInfo {
+public void southIndia()
+{
+	System.out.println("1000");
+
+}
+public void northIndia() {
+	
+	System.out.println("2000");
+}
+public static void main(String[]args) {
+	StateDetails a = new StateDetails();
+	a.southIndia();
+	a.northIndia();
+	a.tamilLanguage();
+	a.englishLanguage();
+	a.hindiLanguge();
+	
+}
+}
